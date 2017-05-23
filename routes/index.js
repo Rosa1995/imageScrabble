24dffaffed16889bf2d9da03c74f577e528afc24
Rosa <sandorosa1@gmail.com>
2017-05-22 18:56:56 -0600

    agregar vista about

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'image Scrabble Game' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {"msg":"Hola Mundo"});
 });
 
 module.exports = router;
